f8bd49872112ce3e26897b0e5317dba5ce8c89f2
Andrew Aguilar <andrew.ronald97@gmail.com>
2019-08-14 14:19:36 -0700

    MVC file structure convert

--- Check It/Controllers/ListViewController.swift
+++ Check It/Controllers/ListViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-    var items = ["Buy tires", "Go try one new balances", "Update resume"]
+    var items = [Item]()
     let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let itemsPList = defaults.array(forKey: "ItemsArray") as? [String]{
+        let newItem = Item()
+        newItem.title="Buy Tires"
+        items.append(newItem)
+        let newItem1 = Item()
+        newItem1.title="Update Resume"
+        items.append(newItem1)
+        let newItem2 = Item()
+        newItem2.title="Get New Shoes"
+        items.append(newItem2)
+        if let itemsPList = defaults.array(forKey: "ItemsArray") as? [Item]{
             items = itemsPList
         }
         // Do any additional setup after loading the view.
@@ -27,7 +36,8 @@ class ListViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath)
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = items[indexPath.row].title
+        cell.accessoryType = items[indexPath.row].complete ? .checkmark : .none
         return cell
         
     }
@@ -37,6 +47,7 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(items[indexPath.row])
         //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        items[indexPath.row].complete = !items[indexPath.row].complete
         if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }else{
@@ -51,11 +62,13 @@ class ListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             print("add item button pressed")
-            print(textField.text)
+            print(textField.text!)
             if(!textField.text!.isEmpty){
-            self.items.append(textField.text!)
-            self.defaults.set(self.items, forKey: "ItemsArray")
-            self.tableView.reloadData()
+                let newItem = Item()
+                newItem.title = textField.text!
+                self.items.append(newItem)
+                self.defaults.set(self.items, forKey: "ItemsArray")
+                self.tableView.reloadData()
             }
         }
         alert.addTextField { (alertTextField) in
@@ -66,6 +79,7 @@ class ListViewController: UITableViewController {
          alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-    
 }
+
+
 
---
+++ Check It/DataModel/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Check It
+//
+//  Created by Andrew Aguilar on 8/14/19.
+//  Copyright © 2019 Andrew Aguilar. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var complete: Bool = false
+}
